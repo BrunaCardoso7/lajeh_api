f23eca12156cc255083fd3c55182e2e1f3302269
BrunaCardoso7 <obitadrawing@gmail.com>
2025-01-20 10:33:58 -0300

    fix(users): corrige problema de conexão assíncrona com banco de dados. (BrunaCardoso7)

--- lajeh_api/database.py
+++ lajeh_api/database.py
@@ -3,9 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from lajeh_api.settings import Settings
+from typing import AsyncGenerator
+
 
 # engine=create_async_engine(Settings().DATABASE_URL, echo=True)
-engine=create_async_engine("postgresql+asyncpg://lajeh_0_1_user:VeLw1y6KniThyeRZrhW005gSq6C4H46i@dpg-cu2t6qlumphs73b0nha0-a.oregon-postgres.render.com/lajeh_0_1", echo=True)
+engine=create_async_engine(
+    "postgresql+asyncpg://lajeh_0_1_user:VeLw1y6KniThyeRZrhW005gSq6C4H46i@dpg-cu2t6qlumphs73b0nha0-a.oregon-postgres.render.com/lajeh_0_1", 
+    echo=True
+)
 
 
 AsyncSessionLocal = sessionmaker(
@@ -17,11 +22,11 @@ AsyncSessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-async def get_db():    
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
 
-
-def check_connection(engine):
-    with engine.connect() as conn:
-        conn.execute(text("SELECT 1"))
+async def check_connection():
+    async with engine.connect() as conn:
+        await conn.execute(text("SELECT 1"))  # Executando um simples SELECT para verificar
+        print("Conexão bem-sucedida com o banco de dados!")
--- lajeh_api/main.py
+++ lajeh_api/main.py
@@ -1,29 +1,25 @@
 
 from fastapi import FastAPI
 from lajeh_api.database import check_connection
-from sqlalchemy import create_engine
-from lajeh_api.settings import Settings
 from lajeh_api.users.routes import router as user_router
 from lajeh_api.fornecedores.controllers import router as fornecedor_router
 from lajeh_api.configs.cloudinary.main import configure_cloudinary
-engine=create_engine(Settings().DATABASE_URL)
+
 
 
 async def setup():
     try:
-        check_connection(engine)
+        await check_connection()
         print("Conexão com o banco de dados estabelecida com sucesso.")
     except Exception as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
 
 
-# Função lifespan
 async def lifespan(app):
     await setup()
     yield
 
 
-# Instância do FastAPI
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
 app.include_router(fornecedor_router)
